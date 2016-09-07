9c703b5c101b3312f417c003bed1536e0805a4d6
Diego  Collao <dcollao@icloud.com>
2016-09-07 05:34:50 -0300

    routes added

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
+//We dont put /about, we are in about so .. isnt necessary
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
--- routes/contact.js
+++ routes/contact.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//We dont put /about, we are in about so .. isnt necessary
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Expressbanana' });
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
